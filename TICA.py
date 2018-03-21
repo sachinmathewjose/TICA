602e45abbe4de2cd6c88d8775ce9533947043d3a
AzureAD\sachinmathewjose <sachin.jose9@gmail.com>
2018-03-21 16:26:22 -0400

    added symbols on the buttons

--- TICA.py
+++ TICA.py
@@ -1,9 +1,10 @@
 import cv2
 import numpy as np
 pts = []
-thickness = 5
+thickness = 25
 drawing = None
 colour = (0,0,255)
+RECTANGLE_LENGTH  = 50; #length of the tool area rectangle
 
 def track(image):
     global pts, thickness, drawing,colour
@@ -31,23 +32,31 @@ def track(image):
         ctr = (centroid_x, centroid_y)
         # Put black circle in at centroid in image
         cv2.circle(image, ctr, 4, (0,0,0))
-        
-    RECTANGLE_LENGTH  = 45; 
+         
     cv2.rectangle(image,(5,5),(RECTANGLE_LENGTH,RECTANGLE_LENGTH),(1,0,0))
+    cv2.rectangle(image,(20,20),(RECTANGLE_LENGTH-15,RECTANGLE_LENGTH-15),(0,0,255),-1)
     cv2.rectangle(image,(5,RECTANGLE_LENGTH),(RECTANGLE_LENGTH,2*RECTANGLE_LENGTH),(1,0,0))
+    cv2.rectangle(image,(5+10,RECTANGLE_LENGTH+20),(RECTANGLE_LENGTH-10,2*RECTANGLE_LENGTH-20),(1,0,0))
+    cv2.rectangle(image,(5+20,RECTANGLE_LENGTH+10),(RECTANGLE_LENGTH-20,2*RECTANGLE_LENGTH-10),(1,0,0))
     cv2.rectangle(image,(5,RECTANGLE_LENGTH*2),(RECTANGLE_LENGTH,RECTANGLE_LENGTH*3),(1,0,0))
+    cv2.rectangle(image,(5+10,RECTANGLE_LENGTH*2+20),(RECTANGLE_LENGTH-10,3*RECTANGLE_LENGTH-20),(1,0,0))
     cv2.rectangle(image,(5,RECTANGLE_LENGTH*3),(RECTANGLE_LENGTH,RECTANGLE_LENGTH*4),(1,0,0))
+    cv2.rectangle(image,(5+15,RECTANGLE_LENGTH*3+15),(RECTANGLE_LENGTH-15,4*RECTANGLE_LENGTH-15),(0,0,255))
 
     if 5<ctr[0]<RECTANGLE_LENGTH and 5<ctr[1]<RECTANGLE_LENGTH: 
         colour = (0,0,255) #colour red
     if 5<ctr[0]<RECTANGLE_LENGTH and RECTANGLE_LENGTH*3<ctr[1]<RECTANGLE_LENGTH*4 :
         colour = (0,0,0) #make colour black
+    if 5<ctr[0]<RECTANGLE_LENGTH and RECTANGLE_LENGTH<ctr[1]<RECTANGLE_LENGTH*2:
+        if (thickness<500):
+            thickness+=1
+    if 5<ctr[0]<RECTANGLE_LENGTH and RECTANGLE_LENGTH*2<ctr[1]<RECTANGLE_LENGTH*3:
+        if (thickness>22):
+            thickness-=1
      
     if ctr is not None and not ((5<ctr[0]<RECTANGLE_LENGTH) and (5<ctr[1]<RECTANGLE_LENGTH*4)):
-        cv2.circle(drawing, ctr, thickness, colour, -1)
-    
-        #cv2.circle(image, pts[i],thickness, (0, 0, 255), -1)
-        #cv2.line(image, pts[i - 1], pts[i], (0, 0, 255), thickness)
+        cv2.circle(drawing, ctr, thickness//25, colour, -1)
+
     # Display full-color image
     image = cv2.add(image, drawing)
     cv2.imshow('CA', image)
